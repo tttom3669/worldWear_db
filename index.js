b9f6a1a75d51411427568239cb0f596e850e5c7c
tttom3669 <tttom3669@gmail.com>
2025-01-26 15:31:23 +0800

    update:自訂路由

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const cors = require('cors');
 // const { v4: uuidv4 } = require('uuid');
 const { nanoid } = require('nanoid');
 const jsonServer = require('json-server');
-
-const server = jsonServer.create();
 const auth = require('json-server-auth');
 
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(cors());
-
 server.use(jsonServer.bodyParser);
+
 // 自訂中介層，為每筆新增資料生成亂碼 ID
 server.use((req, res, next) => {
   if (req.method === 'POST' && req.body) {
@@ -21,7 +21,15 @@ server.use((req, res, next) => {
   next();
 });
 
+const rules = auth.rewriter({
+  users: 600,
+  orders: 600,
+  "/admin/orders/*": '/660/orders'
+});
+
 server.db = router.db;
+
+server.use(rules);
 server.use(auth);
 server.use(router);
 
